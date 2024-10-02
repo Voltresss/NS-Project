4539490d360f395d6353bcbb77db3e41868804e4
Voltresss <actionlobby@gmail.com>
2024-10-02 16:45:03 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+var a = document.getElementById("lo")
+    var b = document.getElementById("log")
+    function login()
+    {
+        if(length.b>8)
+    {
+        console.log("enter more strong password:-")
+    }
+
+    }
